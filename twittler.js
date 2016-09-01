18dcdfd106987583efe3be5f323c2823b6ffaed3
Tim Garibaldi <tgaribal@gmail.com>
2016-09-01 13:19:51 -0700

    user can now load more twittles

--- twittler.js
+++ twittler.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
 
-//make this work that if a user is passed, it will build the tweets of that user, if no user is passed, it will build all available tweets
-//can we use memoize or something? or something similar? see if user already has a group of tweets and use those?
-//make welcome header into a "back" button
-  var populateTweets = function (user) {
-    var $feed = $('.user-feed');
-    $feed.html=('');
+  var createPosts = function () {
+    var $feed = $('.user-feed');  
+    $feed.empty();
     var index = streams.home.length - 1;
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet group"></div>');
-      //var timeStamp = Date.now
-      $tweet.html('<div class="handle"><a data-user='+tweet.user+'>@' + tweet.user + ':</a></div> <div class="message">' + tweet.message +'</div> <div class="time">timestamp</div>');
+      $tweet.html('<div class="handle"><a data-user='+tweet.user+'>@' + tweet.user + ':</a></div> <div class="message">' + tweet.message +'</div> <div class="time">'+tweet.created_at+'</div>');
       $tweet.appendTo($feed);
       index -= 1;
     }
   }
-  populateTweets();
+  createPosts();
+
+  $('.load-more').on('click', createPosts);
+
+  $('.add-twittle').on('click', function (event) {
+    event.preventDefault();
+    var input = $('.twittle').val();
+    //writeTweet(input);
+    var $tweet = $('<div class="tweet group"></div>');
+    if (input.length===0) {
+      alert("Enter a Twittle to join the noise")
+    } else {
+      $tweet.html('<div class="handle"><a data-user=tgaribaldi>@tgaribaldi:</a></div> <div class="message">' + input +'</div> <div class="time">timestamp</div>');
+      $tweet.prependTo('.user-feed');
+    }
+  })
+
+  $('.handle').on('click', function() {
+    alert('click')
+  })
 
   $('.button').on('mouseover', function(){
     $(this).addClass('highlighted')
@@ -25,18 +40,6 @@ $(document).ready(function(){
   $('.button').on('mouseleave', function() {
     $(this).removeClass('highlighted')
   })
-
-  $('.button').on('click', function(event) {
-    event.preventDefault();
-    var $tweet = $('<div class="tweet group"></div>');
-    var input = $('.twittle').val();
-    //var timeStamp = Date.now
-    $tweet.html('<div class="handle"><a data-user=tgaribaldi>@tgaribaldi:</a></div> <div class="message">' + input +'</div> <div class="time">timestamp</div>');
-    $tweet.prependTo('.user-feed');
-    $('.twittle').val('');
-  })
-
-  // $('.tweet').on('click', 'handle', populateTweets);
 
 });
 
